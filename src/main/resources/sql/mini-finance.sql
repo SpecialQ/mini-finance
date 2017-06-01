df2c98dee2f9e385bd2e29bd52f00a8db9c05081
SpecialQ <252051985@qq.com>
2017-06-01 18:17:47 +0800

    数据库模型及sql

---
+++ src/main/resources/sql/mini-finance.sql
@@ -0,0 +1,85 @@
+CREATE TABLE IF NOT EXISTS `mini-finance`.`user` (
+  `id` BIGINT NOT NULL,
+  `username` VARCHAR(32) NOT NULL,
+  `password` VARCHAR(128) NOT NULL,
+  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `enabled` TINYINT NOT NULL DEFAULT 0 COMMENT 'o - 未激活\n1 - 已激活',
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `user_username_UNIQUE` (`username` ASC));
+  
+INSERT INTO `mini-finance`.`user` (`id`, `username`, `password`, `create_time`, `enabled`) VALUES (0, 'admin', '19871129', now(), 1);
+
+CREATE TABLE IF NOT EXISTS `mini-finance`.`user_ext` (
+  `user_id` BIGINT NOT NULL,
+  `phone` VARCHAR(32) NULL,
+  `email` VARCHAR(32) NULL,
+  `address` VARCHAR(64) NULL,
+  `alias` VARCHAR(32) NULL,
+  `sex` CHAR(2) NULL COMMENT 'x - 女\ny - 男\nxy - 不明',
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `fk_ext_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `mini-finance`.`user` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+INSERT INTO `mini-finance`.`user_ext` (`user_id`, `phone`, `email`, `address`, `alias`, `sex`) VALUES (0, '18964749949', '252051985@qq.com', NULL, 'SpecialQ', 'y');
+
+CREATE TABLE IF NOT EXISTS `mini-finance`.`role` (
+  `id` BIGINT NOT NULL,
+  `role_name` VARCHAR(32) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+INSERT INTO `mini-finance`.`role` (`id`, `role_name`) VALUES (0, 'root');
+
+CREATE TABLE IF NOT EXISTS `mini-finance`.`user_role_rela` (
+  `user_id` BIGINT NOT NULL,
+  `role_id` BIGINT NOT NULL,
+  PRIMARY KEY (`user_id`, `role_id`),
+  INDEX `fk_role_id_idx` (`role_id` ASC),
+  CONSTRAINT `fk_rela_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `mini-finance`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_rela_role_id`
+    FOREIGN KEY (`role_id`)
+    REFERENCES `mini-finance`.`role` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+INSERT INTO `mini-finance`.`user_role_rela` (`user_id`, `role_id`) VALUES (0, 0);
+
+CREATE TABLE IF NOT EXISTS `mini-finance`.`authorization` (
+  `id` BIGINT NOT NULL,
+  `path` VARCHAR(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `auth_path_UNIQUE` (`path` ASC))
+ENGINE = InnoDB;
+
+INSERT INTO `mini-finance`.`authorization` (`id`, `path`) VALUES (0, '/*');
+INSERT INTO `mini-finance`.`authorization` (`id`, `path`) VALUES (1, '/**');
+
+CREATE TABLE IF NOT EXISTS `mini-finance`.`role_auth_rela` (
+  `role_id` BIGINT NOT NULL,
+  `auth_id` BIGINT NOT NULL,
+  PRIMARY KEY (`role_id`, `auth_id`),
+  INDEX `fk_auth_id_idx` (`auth_id` ASC),
+  CONSTRAINT `fk_role_id`
+    FOREIGN KEY (`role_id`)
+    REFERENCES `mini-finance`.`role` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_auth_id`
+    FOREIGN KEY (`auth_id`)
+    REFERENCES `mini-finance`.`authorization` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+INSERT INTO `mini-finance`.`role_auth_rela` (`role_id`, `auth_id`) VALUES (0, 0);
+INSERT INTO `mini-finance`.`role_auth_rela` (`role_id`, `auth_id`) VALUES (0, 1);
+
